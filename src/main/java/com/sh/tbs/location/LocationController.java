8a777e2b01438384f179e0a48b3718a98ac95591
Suleman Rawla <SRawla@users.noreply.github.com>
2026-06-06 16:10:28 +0530

    feat: add /info endpoint and request logging to controllers (#1)
    feat: add /info endpoint and request logging to controllers (#1)

--- src/main/java/com/sh/tbs/location/LocationController.java
+++ src/main/java/com/sh/tbs/location/LocationController.java
@@ -4,12 +4,14 @@ import com.sh.tbs.location.dto.LocationRequest;
 import com.sh.tbs.location.dto.LocationResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/locations")
 @RequiredArgsConstructor
@@ -19,29 +21,34 @@ public class LocationController {
 
     @GetMapping
     public List<LocationResponse> findAll() {
+        log.info("GET /locations");
         return service.findAll();
     }
 
     @GetMapping("/{id}")
     public LocationResponse findById(@PathVariable UUID id) {
+        log.info("GET /locations/{}", id);
         return service.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LocationResponse create(@Valid @RequestBody LocationRequest request) {
+        log.info("POST /locations - {}", request.name());
         return service.create(request);
     }
 
     @PutMapping("/{id}")
     public LocationResponse update(@PathVariable UUID id,
                                    @Valid @RequestBody LocationRequest request) {
+        log.info("PUT /locations/{}", id);
         return service.update(id, request);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
+        log.info("DELETE /locations/{}", id);
         service.delete(id);
     }
 }
